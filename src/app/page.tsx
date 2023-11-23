4d6b0ff065b0d4b73658080a367fd4c6a31aaecc
ryotaro-0805 <rasouryo@yahoo.co.jp>
2023-11-23 22:59:26 +0900

    ✨ タイトルを追加しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,9 @@ export default function Home() {
 
   return (
     <div>
-      <h1 className='main_title'>inputタグに打ち込んだ文字を<br />ローカルストレージに保存してみよう</h1>
+      <h1 className='text-3xl text-center my-2'>Input App</h1>
+      <p className='text-xl text-center my-2'>This App is created by <span className='text-xl text-violet-800 hover:cursor-pointer'>~Ryotaro~</span></p>
+      <h2 className='main_title'>inputタグに打ち込んだ文字を<br />ローカルストレージに保存してみよう</h2>
       <form onSubmit={handleSubmit} className='my-5 text-center' action="">
         <input ref={textRef} type="text" className='w-64 h-10 px-4 py-2 bg-gray-200 border rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500' placeholder={input_placefolder_text} />
         <button type='submit' id="myButton" className="bg-blue-500 hover:bg-blue-600 text-white font-bold mx-5 py-2 px-4 rounded transition duration-300">
@@ -55,7 +57,7 @@ export default function Home() {
       <div className='text-center'>
         {getText ?
           <div>
-            <p>{`現在ローカルストレージには${getText}というデータが保存されています。`}</p>
+            <p>{`現在ローカルストレージには『${getText}』というデータが保存されています。`}</p>
             <button onClick={handleClear} id="myButton" className="bg-pink-500 hover:bg-pink-600 text-white font-bold mx-5 py-1 px-2 rounded transition duration-300">
               CLEAE
             </button>
