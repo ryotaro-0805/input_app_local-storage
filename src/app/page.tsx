a931b5a3ba81d5756e1d4c9eb92b48288e983d87
ryotaro-0805 <rasouryo@yahoo.co.jp>
2023-11-26 12:27:20 +0900

    ✅ 複数のデータを保存できるようにしました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,36 @@ import { useEffect, useRef, useState } from 'react';
 export default function Home() {
   const input_placefolder_text: string = '入力してください';
   const textRef = useRef<any>('');
-  const [getText, setGetText] = useState<any>(null);
+  const [getText, setGetText] = useState<any>(['']);
   const [currentStatus, setCurrentStatus] = useState<string>('ロード中です。');
 
   useEffect(() => {
-    setGetText(localStorage.getItem('storagekey'));
-    if (!getText) {
+    const getArray = localStorage.getItem('storagekey');
+    if (getArray !== null) {
+      setGetText(JSON.parse(getArray));
+    } else {
       setCurrentStatus('現在ローカルストレージにはデータは保存されていません。')
     }
   }, []);
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    textRef.current.value.length ? setGetText(textRef.current?.value) : null;
+    textRef.current.value.length ? setGetText((e: any) => [...e, textRef.current?.value]) : null;
   }
 
+  // useEffect(() => {
+  //   const dataGeter: any = localStorage.getItem('storagekey');
+  //   setGetText(dataGeter);
+  //   console.log(getText);
+
+  // }, []);
+
+
   useEffect(() => {
-    const dataGeter: any = localStorage.getItem('storagekey');
-    setGetText(dataGeter);
     console.log(getText);
-
-  }, []);
-
-
-  useEffect(() => {
-    console.log(textRef.current.value.length);
     textRef.current.value = '';
     getText ?
-      localStorage.setItem('storagekey', getText)
+      localStorage.setItem('storagekey', JSON.stringify(getText))
       : null
   }, [getText]);
 
@@ -41,11 +43,10 @@ export default function Home() {
     localStorage.setItem('storagekey', '')
   }
 
-
   return (
     <div>
       <h1 className='text-3xl text-center my-2'>Input App</h1>
-      <p className='text-xl text-center my-2'>This App is created by <span className='text-xl text-violet-800 hover:cursor-pointer'>~Ryotaro~</span></p>
+      <p className='text-xl text-center my-2'>This App is created by <span className='text-xl text-pink-400 hover:cursor-pointer'>~Ryotaro~</span></p>
       <h2 className='main_title'>inputタグに打ち込んだ文字を<br />ローカルストレージに保存してみよう</h2>
       <form onSubmit={handleSubmit} className='my-5 text-center' action="">
         <input ref={textRef} type="text" className='w-64 h-10 px-4 py-2 bg-gray-200 border rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500' placeholder={input_placefolder_text} />
@@ -57,13 +58,18 @@ export default function Home() {
       <div className='text-center'>
         {getText ?
           <div>
-            <p>{`現在ローカルストレージには『${getText}』というデータが保存されています。`}</p>
+            <p>現在ローカルストレージには以下のデーターが保存されています。</p>
+            {getText.map((data: any, index: number) => (
+              <div>
+                <p className='m-auto text-left w-64' key={index}>{`${index + 1}. ${data}`}</p>
+                <hr className='m-auto text-left w-64' />
+              </div>
+            ))}
             <button onClick={handleClear} id="myButton" className="bg-pink-500 hover:bg-pink-600 text-white font-bold mx-5 py-1 px-2 rounded transition duration-300">
               CLEAE
             </button>
           </div>
           :
-          // <p>{`現在ローカルストレージにはデータは保存されていません。`}</p>
           <p>{currentStatus}</p>
         }
       </div>
